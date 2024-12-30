ef1dd0d4fb88e5f9a9070fe68c79a14a6deca738
TasXI <86817885+TasXI@users.noreply.github.com>
2024-12-30 11:19:27 +0200

    nn
    a

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    id("com.gradleup.shadow") version "8.3.1"
+}
+
+
+group = "com.example"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+
+
+dependencies {
+
+    implementation(libs.ktor.web.sockets)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.mongodb.driver.core)
+    implementation(libs.mongodb.driver.sync)
+    implementation(libs.bson)
+    implementation(libs.ktor.server.rate.limiting)
+    implementation(libs.ktor.serialization.gson)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
